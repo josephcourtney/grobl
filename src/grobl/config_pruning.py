0b12713be2c2efe0491c38b49ae486acf0534e97
Joseph Courtney <5942706+josephcourtney@users.noreply.github.com>
2026-09-22 12:46:58 -0400

    Format config discovery imports

--- src/grobl/config_pruning.py
+++ src/grobl/config_pruning.py
@@ -13,7 +13,12 @@ from tomlkit.exceptions import TOMLKitError
 from tomlkit.items import Array
 
 from grobl.config_defaults import TOML_CONFIG, load_default_config
-from grobl.config_loading import discover_grobl_toml_files, LEGACY_TOML_CONFIG, PYPROJECT_TOML, load_toml_config
+from grobl.config_loading import (
+    LEGACY_TOML_CONFIG,
+    PYPROJECT_TOML,
+    discover_grobl_toml_files,
+    load_toml_config,
+)
 from grobl.constants import (
     CONFIG_EXCLUDE,
     CONFIG_EXCLUDE_CONTENT,
